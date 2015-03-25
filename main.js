895d5df6052fac50380352ba0609fabce02155d0
Mike Pacheco <mikelnpacheco@gmail.com>
2015-03-24 20:42:09 -0700

    basic game complete

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 var holes = document.querySelectorAll(".hole");
-
-	var randomBill = function(){
-	holes[Math.floor(Math.random() * (holes.length ))].innerHTML = "<img src=http://www.lionsroar.com/wp-content/uploads/2009/05/groundhog-day-driving.jpg>"
+var moleCounter = 0;
+var score = document.querySelectorAll("h4")[0];
+function randomNumber(){
+	  return Math.floor(Math.random() * (holes.length ));
+	 
 }
-// var randomBill = function(){
+holes[randomNumber()].innerHTML = "<img src = http://www.lionsroar.com/wp-content/uploads/2009/05/groundhog-day-driving.jpg>";
 
-// holes[randomBill].innerHTML = "<img src=http://www.lionsroar.com/wp-content/uploads/2009/05/groundhog-day-driving.jpg>"
-// }
+
+	for (var j=0; j< holes.length; j++) {
+		holes[j].addEventListener("click", function(){
+		if (this.innerHTML !== ""){
+			this.innerHTML = "";
+			moleCounter++;
+			score.innerText = moleCounter;
+			holes[randomNumber()].innerHTML = "<img src = http://www.lionsroar.com/wp-content/uploads/2009/05/groundhog-day-driving.jpg>";
+		}
+	})
+};
+
+// need to add and event listener 
